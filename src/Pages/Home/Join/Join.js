0c04e47ba08c1001b2ffcf65be025693c27e09ea
farzanapomy <farzanapomy56@gmail.com>
2021-10-20 22:46:25 +0600

    updated

--- src/Pages/Home/Join/Join.js
+++ src/Pages/Home/Join/Join.js
@@ -6,12 +6,24 @@ const Join = () => {
         <div className='container text-start my-5'>
             <form class="row g-3">
                 <div class="col-md-6">
+                    <label for="inputEmail4" class="form-label">You First Name</label>
+                    <input required type="text" class="form-control" id="inputEmail4" />
+                </div>
+                <div class="col-md-6">
+                    <label for="inputEmail4" class="form-label">You Last Name</label>
+                    <input required type="text" class="form-control" id="inputEmail4" />
+                </div>
+                <div class="col-md-6">
                     <label for="inputEmail4" class="form-label">Email</label>
                     <input required type="email" class="form-control" id="inputEmail4" />
                 </div>
                 <div class="col-md-6">
                     <label for="inputPassword4" class="form-label">Password</label>
                     <input required type="password" class="form-control" id="inputPassword4" />
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Write About Your qualifications</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div>
                 <div class="col-12">
                     <label for="inputAddress" class="form-label">Address</label>
@@ -36,14 +48,7 @@ const Join = () => {
                     <label for="inputZip" class="form-label">Zip</label>
                     <input required type="text" class="form-control" id="inputZip" />
                 </div>
-                <div class="col-12">
-                    <div class="form-check">
-                        <input required class="form-check-input" type="checkbox" id="gridCheck" />
-                        <label class="form-check-label" for="gridCheck">
-                            Check me out
-                        </label>
-                    </div>
-                </div>
+                
                 <div class="col-12">
                     <Link to='successful'> <button type="submit" class="btn btn-primary">Sign in</button>
                     </Link>
--- src/Pages/LiveTreatment/LiveTreatment.js
+++ src/Pages/LiveTreatment/LiveTreatment.js
@@ -8,7 +8,7 @@ const LiveTreatment = () => {
                 <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
             </div>
             <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                <label for="exampleFormControlTextarea1" class="form-label">Write details</label>
                 <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
             </div>
             <select class="form-select" aria-label="Default select example">
